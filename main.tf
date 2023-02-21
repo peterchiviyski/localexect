45e7760de81848a58369e5df6ec0bc7d41bc02b2
peterchiviyski <peter.chiviyski@hashicorp.com>
2023-02-21 16:27:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@
 
 resource "null_resource" "install_gcloud" {
   provisioner "local-exec" {
-    command = "curl https://sdk.cloud.google.com | bash && exec -l $SHELL"
+    command = "curl https://sdk.cloud.google.com | bash && exec $SHELL"
   }
 }
